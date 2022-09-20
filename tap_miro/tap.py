4c61a0af2da05e8207bf40710f51ba1f3ff2cc54
Robby Robinson <robby.robinson@slalom.com>
2022-09-19 19:13:47 -0700

    tap setup

--- tap_miro/tap.py
+++ tap_miro/tap.py
@@ -6,15 +6,11 @@ from singer_sdk import Tap, Stream
 from singer_sdk import typing as th  # JSON schema typing helpers
 # TODO: Import your custom stream types here:
 from tap_miro.streams import (
-    MiroStream,
-    UsersStream,
-    GroupsStream,
+    OrgMembersStream,
 )
-# TODO: Compile a list of custom stream types here
-#       OR rewrite discover_streams() below with your custom logic.
+
 STREAM_TYPES = [
-    UsersStream,
-    GroupsStream,
+    OrgMembersStream
 ]
 
 
@@ -22,31 +18,49 @@ class TapMiro(Tap):
     """Miro tap class."""
     name = "tap-miro"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
+#        th.Property(
+#            "client_id",
+#            th.StringType,
+#            required=False,
+#            description="The token to authenticate against the API service"
+#        ),
+#        th.Property(
+#            "client_secret",
+#            th.ArrayType(th.StringType),
+#            required=False,
+#            description="Project IDs to replicate"
+#        ),
+#        th.Property(
+#            "auth_code",
+#            th.DateTimeType,
+#            required=False,
+#            description="The earliest record date to sync"
+#        ),
+#        th.Property(
+#            "redirect_uri",
+#            th.StringType,
+#            required=False,
+#            description="The url for the API service"
+#        ),
         th.Property(
-            "auth_token",
+            "access_token",
             th.StringType,
             required=True,
-            description="The token to authenticate against the API service"
-        ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate"
-        ),
-        th.Property(
-            "start_date",
-            th.DateTimeType,
-            description="The earliest record date to sync"
-        ),
-        th.Property(
-            "api_url",
-            th.StringType,
-            default="https://api.mysample.com",
             description="The url for the API service"
         ),
+        th.Property(
+            "org_id",
+            th.StringType,
+            required=True,
+            description="The url for the API service"
+        ),
+#        th.Property(
+#            "team_id",
+#            th.StringType,
+#            required=True,
+#            description="The url for the API service"
+#        ),
     ).to_dict()
 
     def discover_streams(self) -> List[Stream]:

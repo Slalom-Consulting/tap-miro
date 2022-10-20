ff9e5e85994e44e45553169b41ae7202c90458e0
Robby Robinson <robby.robinson@slalom.com>
2022-10-20 15:59:57 -0700

    formatting

--- tap_miro/client.py
+++ tap_miro/client.py
@@ -1,12 +1,11 @@
 """REST client handling, including MiroStream base class."""
 
 from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
 from singer_sdk.streams import RESTStream
 from singer_sdk.authenticators import BearerTokenAuthenticator
 from singer_sdk.pagination import JSONPathPaginator, BaseAPIPaginator
 
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+SCHEMAS_DIR = Path(__file__).parent / Path('./schemas')
 
 class MiroStream(RESTStream):
     """Miro stream class."""
@@ -20,20 +19,16 @@ class MiroStream(RESTStream):
     @property
     def authenticator(self) -> BearerTokenAuthenticator:
         """Return a new authenticator object."""
-        return BearerTokenAuthenticator.create_for_stream(
-            self,
-            token=self.config.get("access_token")
-        )
+        access_token = self.config.get('access_token')
+        return BearerTokenAuthenticator.create_for_stream(self, access_token)
 
     @property
     def http_headers(self) -> dict:
         """Return the http headers needed."""
-        headers = {
-            "Accept": "application/json"
-        }
+        headers = {'Accept': 'application/json'}
 
         if "user_agent" in self.config:
-            headers["User-Agent"] = self.config.get("user_agent")
+            headers['User-Agent'] = self.config.get('user_agent')
             
         return headers
 
--- tap_miro/streams.py
+++ tap_miro/streams.py
@@ -3,20 +3,20 @@
 from pathlib import Path
 from tap_miro.client import MiroStream
 
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+SCHEMAS_DIR = Path(__file__).parent / Path('./schemas')
 
 class OrgMembersStream(MiroStream):
     """Define custom stream."""
-    name = "organization members"
-    path = "/v2/orgs/{org_id}/members"
-    primary_keys = ["id"]
+    name = 'organization members'
+    path = '/v2/orgs/{org_id}/members'
+    primary_keys = ['id']
     replication_key = None
-    schema_filepath = SCHEMAS_DIR / "org_members.json"
-    records_jsonpath = "$.data[*]"
-    cursor_field = "id"
+    schema_filepath = SCHEMAS_DIR / 'org_members.json'
+    records_jsonpath = '$.data[*]'
+    cursor_field = 'id'
     query = {
-        "active": True,
-        "license": "full",
-        "limit": 100,
-        "role": "organization_internal_user",
+        'active': True,
+        'license': 'full',
+        'limit': 100,
+        'role': 'organization_internal_user',
     }

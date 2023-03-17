486022a39f3dfc6b47d7de3d343f930c87f5f88c
Robby Robinson <robby.robinson@slalom.com>
2023-03-17 00:16:09 -0700

    added stream parameter test

--- tap_miro/tests/mock_api.py
+++ tap_miro/tests/mock_api.py
@@ -42,3 +42,40 @@ def mock_api(func, SAMPLE_CONFIG):
             func()
 
     wrapper()
+
+
+mock_param_config = {
+    "organization_members": {
+        "type": "stream",
+        "endpoint": "/v2/orgs/{organization_id}/members?active=true&license=full",
+        "file": "organization_members_parameter.json",
+    }
+}
+
+
+def mock_param_api(func, SAMPLE_CONFIG):
+    """Mock API."""
+    mock_config = mock_param_config
+
+    def wrapper():
+        with requests_mock.Mocker() as m:
+            for k, v in mock_config.items():
+                path = f"{mock_responses_path}/{v['file']}"
+
+                if v["type"] == "stream":
+                    endpoint = v["endpoint"]
+                    for k, v in SAMPLE_CONFIG.items():
+                        var = f"{{{k}}}"
+                        if var in endpoint:
+                            endpoint = endpoint.replace(var, v)
+
+                    url = f"{API_URL}{endpoint}"
+
+                    with open(path, "r") as f:
+                        response = json.load(f)
+
+                    m.get(url, json=response)
+
+            func()
+
+    wrapper()
